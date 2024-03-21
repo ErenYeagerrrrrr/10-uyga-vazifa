706586aa69550396c433fa8c68c1fa17c32bb7e0
Muhammadjon <157690736+ErenYeagerrrrrr@users.noreply.github.com>
2024-03-21 06:20:54 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,65 @@
-let arr = ["ih32ort48rygfq83irf"]
+// // 1-masala==========================================
 
+
+// let arr = [2,3,4,5,45,6,7,767,8,887,9,]
+
+// const x = (a) =>{
+//     let result = [0]
+//     let result2 = [100]
+//     for (let i = 0; i < a.length; i++){
+//         if (result < a[i]){
+//             result = a[i];
+//         }
+//     }
+//     for (let i = 0; i < a.length; i++){
+//         if (result2 > a[i]){
+//             result2 = a[i]
+//         }
+//     }    
+//     return {
+//         katta: result,
+//         kichik: result2,
+//     }
+// }
+
+// console.log(x(arr));
+
+// // 2-masala==================================================
+
+// let arr = [1,2,3,4,546,56,5,676,7,6,23,435,2,1,43,4]
+
+// const x = (a) => {
+//     let result = [];
+//     for (let i = 0; i < a.length; i++){
+//         if (a[i] > 2){
+//             result.push(a[i])
+//         }
+//     }
+//     return result;
+// }
+
+// console.log(x(arr));
+
+// // 3-masala=============================================================
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+
+// const x = (a) => {
+//     let result = [0]
+//     let result2 = [0]
+//     for (let i = 0; i < a.length; i++){
+//         if (a[i] % 2 == 0){
+//             result ++
+//         }
+//         else if (a[i] % 2 == 1){
+//             result2 ++
+//         }
+//     }
+
+//     return {
+//         katta: result,
+//         kichik: result2,
+//     }
+// }
+
+// console.log(x(arr));
